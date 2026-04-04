892d7b5ede24cf616405ffa6346fa5f282e17087
Юрий <sukonkin.jurii@gmail.com>
2026-04-04 14:35:24 +0600

    Добавьте файлы проекта.

---
+++ app/Service/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+
+namespace app.Service
+{
+    class DataService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task LoadData()
+        {
+            string url = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+            using (client)
+            {
+                try
+                {
+                    string jsonString = await client.GetStringAsync(url);
+                    var option = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Ошибка загрузки данных: {e.Message}");
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ app/models/Money.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app.models
+{
+    class Money
+    {
+        public string ID { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public decimal Previous { get; set; }
+    }
+}
