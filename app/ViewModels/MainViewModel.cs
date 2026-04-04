2da356e3208220b56562119da5bc98e00aaee873
Юрий <sukonkin.jurii@gmail.com>
2026-04-04 17:34:25 +0600

    загрузка данных

---
+++ app/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using app.Service;
+using app.models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace app.ViewModels
+{
+    public partial class MainViewModel : ObservableObject 
+    {
+        public MainViewModel() { }
+        private readonly DataService _dataService = new DataService();
+
+        [ObservableProperty]
+        private ObservableCollection<Money> _moneyList = new();
+
+        [RelayCommand]
+        private async Task RefreshData()
+        {
+            var data = await _dataService.LoadData();
+            _moneyList.Clear();
+            foreach (var item in data)
+            {
+                _moneyList.Add(item);
+            }
+        }
+    }
+}
