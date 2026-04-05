64ce88437ae1bc8f96bcea863c153b62a5ebe692
Юрий <sukonkin.jurii@gmail.com>
2026-04-06 02:37:08 +0600

    Добавлены оповещения при удалении и корректировка при добавлении данных

--- app/AddMoneyWindow.xaml.cs
+++ app/AddMoneyWindow.xaml.cs
@@ -1,26 +1,70 @@
 ﻿using app.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace app
 {
-    /// <summary>
-    /// Логика взаимодействия для AddMoney.xaml
-    /// </summary>
     public partial class AddMoney : Window
     {
+        private static readonly Regex _regex = new Regex(@"^[0-9]*[.,]?[0-9]*$");
+
         public AddMoney()
         {
             InitializeComponent();
+        }
+        private bool IsTextAllow(string text)
+        {
+            return _regex.IsMatch(text);
+        }
+
+        private void Value_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            string currentText = textBox.Text;
+            if (textBox.SelectionLength > 0)
+            {
+                currentText = currentText.Remove(textBox.SelectionStart, textBox.SelectionLength);
+            }
+
+            string fullText = currentText.Insert(textBox.SelectionStart, e.Text);
+
+            if (fullText == "." || fullText == ",")
+            {
+                e.Handled = false;
+                return;
+            }
+
+            e.Handled = !IsTextAllow(fullText);
+        }
+        private void Value_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pasteText = (string)e.DataObject.GetData(typeof(string));
+                var textBox = sender as TextBox;
+
+                string currentText = textBox.Text;
+                if (textBox.SelectionLength > 0)
+                {
+                    currentText = currentText.Remove(textBox.SelectionStart, textBox.SelectionLength);
+                }
+
+                string fullText = currentText.Insert(textBox.SelectionStart, pasteText);
+
+                if (!IsTextAllow(fullText))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
         }
     }
 }
